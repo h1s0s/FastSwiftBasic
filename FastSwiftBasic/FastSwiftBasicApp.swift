998829e6cada13a1dd5789c2ad26a5cb7712deba
한상선 <hs94122@naver.com>
2024-02-17 14:04:21 +0900

    Initial Commit

---
+++ FastSwiftBasic/FastSwiftBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastSwiftBasicApp.swift
+//  FastSwiftBasic
+//
+//  Created by 한상선 on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct FastSwiftBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
